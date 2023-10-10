5b5db7190e916f49b4415295fdc727f602f583a6
rabiaakkus <145451092+rabiaakkus@users.noreply.github.com>
2023-10-10 15:14:29 +0300

    Add files via upload

---
+++ Global değişken örnekleri1120/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Global_değişken_örnekleri1120
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTopla = new System.Windows.Forms.Button();
+            this.lblBakiye = new System.Windows.Forms.Label();
+            this.btnOde = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnTopla
+            // 
+            this.btnTopla.Location = new System.Drawing.Point(35, 23);
+            this.btnTopla.Name = "btnTopla";
+            this.btnTopla.Size = new System.Drawing.Size(82, 78);
+            this.btnTopla.TabIndex = 0;
+            this.btnTopla.Text = "TOPLA";
+            this.btnTopla.UseVisualStyleBackColor = true;
+            this.btnTopla.Click += new System.EventHandler(this.btnTopla_Click);
+            // 
+            // lblBakiye
+            // 
+            this.lblBakiye.AutoSize = true;
+            this.lblBakiye.Location = new System.Drawing.Point(69, 138);
+            this.lblBakiye.Name = "lblBakiye";
+            this.lblBakiye.Size = new System.Drawing.Size(35, 13);
+            this.lblBakiye.TabIndex = 1;
+            this.lblBakiye.Text = "label1";
+            // 
+            // btnOde
+            // 
+            this.btnOde.Location = new System.Drawing.Point(166, 29);
+            this.btnOde.Name = "btnOde";
+            this.btnOde.Size = new System.Drawing.Size(75, 67);
+            this.btnOde.TabIndex = 2;
+            this.btnOde.Text = "ode";
+            this.btnOde.UseVisualStyleBackColor = true;
+            this.btnOde.Click += new System.EventHandler(this.btnOde_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(306, 229);
+            this.Controls.Add(this.btnOde);
+            this.Controls.Add(this.lblBakiye);
+            this.Controls.Add(this.btnTopla);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTopla;
+        private System.Windows.Forms.Label lblBakiye;
+        private System.Windows.Forms.Button btnOde;
+    }
+}
+
---
+++ Global değişken örnekleri1120/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Global_değişken_örnekleri1120
+{
+    public partial class Form1 : Form
+    {
+        //bakiye değiskenler globaldır
+       
+        Double bakiye = 200;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTopla_Click(object sender, EventArgs e)
+        {
+        bakiye = bakiye + 50;
+            MessageBox.Show(bakiye.ToString());
+            lblBakiye.Text = "bakiye : " + bakiye.ToString();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblBakiye.Text = "bakiye:" + bakiye.ToString();
+        }
+
+        private void btnOde_Click(object sender, EventArgs e)
+        {
+            bakiye -= 8;
+            lblBakiye.Text = "ode:" + bakiye.ToString();
+
+        }
+    }
+}
